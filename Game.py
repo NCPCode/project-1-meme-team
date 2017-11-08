761065d866176eb89e6a68ca39aa408e0c908741
Student <CSLAB313-1740@instr.cps.k12.il.us>
2017-11-08 16:00:08 -0600

    changed game.py

--- Game.py
+++ Game.py
@@ -1,1 +1,8 @@
 import pygame
+
+window = pygame.display.set_mode((800,500))
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			exit()
+	window.fill(pygame.Color("deepskyblue"))
