d28b2799ecbe4f0348772ba74d243ae779a3b56f
Student <CSLAB313-1740@instr.cps.k12.il.us>
2017-11-08 16:25:06 -0600

    pushing shit (Ryan's shit is pretty bad shit)

--- Game.py
+++ Game.py
@@ -1,8 +1,20 @@
 import pygame
 
 window = pygame.display.set_mode((800,500))
+clock = pygame.time.Clock()
+grass = pygame.Rect(0,400,800,100)
+
 while True:
+	#collectr events
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit()
+	# processing 
+
+	#display 
 	window.fill(pygame.Color("deepskyblue"))
+	grass.fill(pygame.Color("seagreen"))
+	pygame.display.update()
+	clock.tick(60)
+
+
